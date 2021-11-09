308878c7afed08e77c6bcf17b72dbdcd7a3cfbba
SuperSign64 <lazersasha06@gmail.com>
2021-11-08 16:19:57 -0800

    tweaked the lua like 20 times to get the right positioning

--- mods/stages/doobStore.lua
+++ mods/stages/doobStore.lua
@@ -1,6 +1,6 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('actualBg', 'store', 0, 1000)
+	makeLuaSprite('actualBg', 'doobbg/store', -470, -236)
 	addLuaSprite('actualBg', false)
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
